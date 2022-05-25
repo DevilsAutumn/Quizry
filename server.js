cff935b1833a8c79aec7437fd22e02b7f52b0e1b
DevilsAutumn <bhuvnesh875@gmail.com>
2022-05-26 01:10:47 +0530

    initial commit

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongoose.connect(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app / use(express.static("client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
